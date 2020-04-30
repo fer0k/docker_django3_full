f7331aa06dc284381ea4c3f37fc6e8fb4b90e944
Stepan Prohorenkov <sml.ferok@gmail.com>
2020-05-01 02:02:11 +0300

    mediafiles

--- app/blogon/settings.py
+++ app/blogon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "upload",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
